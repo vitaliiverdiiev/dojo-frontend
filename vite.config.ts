f734c0c850cdffeee53db04ec88a0b562461aad3
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2024-08-03 03:40:07 +0300

    trying again #13

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cicd-react/",
+  base: "/dojo-frontend/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
