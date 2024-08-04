7e539fe40cbdab15b16ae0b47d6f1ffea59e85a1
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2024-08-05 02:55:24 +0300

    console.log baseUrl

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,9 +1,12 @@
 import axiosClient from "axios";
 
 const token = localStorage.getItem("token") || "";
+const baseURL = import.meta.env.VITE_API_URL;
+
+console.log({ baseURL });
 
 const axios = axiosClient.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL,
   timeout: 1000,
   headers: {
     Authorization: "Bearer " + JSON.parse(token),
