8c2ec062089716cb0505d537c0eb71b2c6f2c288
vitaliiverdiiev <vitaliiverdiiev@gmail.com>
2024-08-05 03:15:54 +0300

    add: define to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   base: "/dojo-frontend/",
+  define: {
+    "process.env": {
+      VITE_API_URI: process.env.VITE_API_URI,
+      VITE_BASE_URL: process.env.VITE_BASE_URL,
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
